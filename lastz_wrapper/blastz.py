f2401c6982ae90b670db87c4d01068f8cb1b90db
Haibao Tang <tanghaibao@gmail.com>
2010-05-30 19:52:57 -0700

    don't write to intermediate files

--- lastz_wrapper/blastz.py
+++ lastz_wrapper/blastz.py
@@ -5,37 +5,30 @@
 Make LASTZ follow the BLAST convention
 """
 
-import re
 import os
 import sys
-from subprocess import Popen 
+from subprocess import Popen, PIPE
 
-non_standard = re.compile("[^ATGCNatgcn]")
 
-def lastz_to_blast(tmp_file, out_fh=sys.stdout):
-    fp = file(tmp_file)
-    for row in fp:
-        atoms = row.strip().split("\t")
-        name1, name2, coverage, identity, nmismatch, ngap, \
-                start1, end1, start2, end2, score = atoms
-        identity = identity.replace("%", "")
-        hitlen = coverage.split("/")[1]
-        # TODO: evalue formula lookup
-        evalue = str(1e-50)
-        print >>out_fh,  "\t".join((name1, name2, identity, hitlen, nmismatch, ngap, \
-                start1, end1, start2, end2, evalue, score))
+def lastz_to_blast(row):
+    atoms = row.strip().split("\t")
+    name1, name2, coverage, identity, nmismatch, ngap, \
+            start1, end1, start2, end2, score = atoms
+    identity = identity.replace("%", "")
+    hitlen = coverage.split("/")[1]
+    # TODO: evalue formula lookup
+    evalue = str(1e-50)
+    return "\t".join((name1, name2, identity, hitlen, nmismatch, ngap, \
+            start1, end1, start2, end2, evalue, score))
 
 
 def sh(cmd, out_fh=sys.stdout):
     print >>sys.stderr, cmd
-    proc = Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, shell=True)
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
     print >>sys.stderr, "job sent to <%d>" % proc.pid
-    out, err = proc.communicate()
+    for row in proc.stdout:
+        print >>out_fh, lastz_to_blast(row)
     print >>sys.stderr, "job <%d> finished" % proc.pid
-
-
-def get_base_name(st):
-    return os.path.basename(st).split(".")[0]
 
 
 if __name__ == '__main__':
@@ -65,11 +58,8 @@ if __name__ == '__main__':
     if options.query is None or options.target is None:
         sys.exit(parser.print_help())
 
-    tmp_fn = "%s_%s.running.blastz" % (get_base_name(afasta_fn), get_base_name(bfasta_fn))
-
-    lastz_cmd = "lastz --format=general-:%s --transition=2 --ambiguous=iupac %s[multiple] %s --output=%s"
-    lastz_cmd = lastz_cmd % (lastz_fields, bfasta_fn, afasta_fn, tmp_fn) 
+    lastz_cmd = "lastz --format=general-:%s --transition=2 --ambiguous=iupac %s[multiple] %s"
+    lastz_cmd = lastz_cmd % (lastz_fields, bfasta_fn, afasta_fn) 
 
     sh(lastz_cmd, out_fh=out_fh)
-    lastz_to_blast(tmp_fn, out_fh=out_fh)
 
